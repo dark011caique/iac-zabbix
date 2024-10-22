ea777ef3881ca26470f101cc7309bb2e99d0419f
dark011caique <caique.quiky@gmail.com>
2024-10-22 13:42:00 -0300

    projeto finalizado e funcional

--- infra/Grupo_de_Segurança.tf
+++ infra/Grupo_de_Segurança.tf
@@ -1,20 +1,34 @@
 resource "aws_security_group" "acesso_geral" {
-  name = "acesso_geral"
-  description = "grupo do Dev"
-  ingress{
-      cidr_blocks = [ "0.0.0.0/0" ]
-      ipv6_cidr_blocks = [ "::/0" ]
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
+  name        = "acesso_geral"
+  description = "Grupo"
+  
+  ingress {
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [ "0.0.0.0/0" ] # Troque pelo seu IP ou utilize 0.0.0.0/0 para liberar para todos (não recomendado em produção)
+    ipv6_cidr_blocks = [ "::/0" ]
   }
-  egress{
-      cidr_blocks = [ "0.0.0.0/0" ]
-      ipv6_cidr_blocks = [ "::/0" ]
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
+
+  ingress {
+    description      = "Allow all inbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"    # Liberando todo o tráfego de entrada
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
   }
+
+  egress {
+    description      = "Allow all outbound traffic"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"    # Liberando todo o tráfego de saída
+    cidr_blocks      = [ "0.0.0.0/0" ]
+    ipv6_cidr_blocks = [ "::/0" ]
+  }
+
   tags = {
     Name = "acesso_geral"
   }
